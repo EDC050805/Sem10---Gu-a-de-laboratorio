f0735ff8dbe2b3e3dcb7fa303b4e47cb78138e1e
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-10-25 05:20:22 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Sem10___Guía_de_laboratorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem10___Guía_de_laboratorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tTienda de celulares del Sr. Luis");
+            Console.WriteLine("\t********************************\n");
+            int opcion, VmPhone = 0, VmPad = 0, VMAPBrook = 0, VmWatch = 0, DmPhone = 0, DmPad = 0, DMAPBrook = 0, DmWatch = 0;
+            do
+            {
+                Console.Clear();
+                opcion = InterfazConsola.PantalladeInicio();
+                string producto = "mPhone 3000";
+
+                switch (opcion)
+                {
+                    case 1:
+                        InterfazConsola.Producto(producto, ref VmPhone, ref VmPad, ref VMAPBrook, ref VmWatch, ref DmPhone, ref DmPad, 
+                            ref DMAPBrook, ref DmWatch);
+                        break;
+                    case 2:
+                        producto = "mPad 3500";
+                        InterfazConsola.Producto(producto, ref VmPhone, ref VmPad, ref VMAPBrook, ref VmWatch, ref DmPhone, ref DmPad, 
+                            ref DMAPBrook, ref DmWatch);
+                        break;
+                    case 3:
+                        producto = "MAPBrook 3800";
+                        InterfazConsola.Producto(producto, ref VmPhone, ref VmPad, ref VMAPBrook, ref VmWatch, ref DmPhone, ref DmPad, 
+                            ref DMAPBrook, ref DmWatch);
+                        break;
+                    case 4:
+                        producto = "mWatch 8000";
+                        InterfazConsola.Producto(producto, ref VmPhone, ref VmPad, ref VMAPBrook, ref VmWatch, ref DmPhone, ref DmPad, 
+                            ref DMAPBrook, ref DmWatch);
+                        break;
+                }
+
+            } while (opcion != 5);
+
+            int mPhone = VmPhone - DmPhone;
+            int mPad = VmPad - DmPad;
+            int MAPBrook = VMAPBrook - DMAPBrook;
+            int mWatch = VmWatch - DmWatch;
+            InterfazConsola.Reporte(ref mPhone, ref mPad, ref MAPBrook, ref mWatch);
+            Console.ReadKey();
+        }
+    }
+}
